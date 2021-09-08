4f375027147642ae4ce23136d6845a312ba046fd
Diego <diegovillarreal123@gmail.com>
2021-09-08 03:07:43 -0500

    updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,79 +1,200 @@
-// Assignment Code
+// Button to prompt the message
 var generateBtn = document.querySelector("#generate");
 
-// Generate password function 
+// Write an array string
+var characterOptions = [];
+
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var special = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  ":",
+  ";",
+  "'",
+  "",
+  "<",
+  ",",
+  ">",
+  ".",
+  "?",
+  "/",
+  "|",
+  "{",
+  "}",
+  "[",
+  "]",
+  "<",
+  ">",
+];
+
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// Generate password options function
 function generatePassword(
-    passwordLength,
-    includeNumeric,
-    includeSpecial,
-    includeLowercase,
-    includeUppercase
+  passwordLength,
+  numericCharacters,
+  specialCharacters,
+  lowerCaseCharacters,
+  upperCaseCharacters
 ) {
+  // First window alert options
+  passwordLength = window.prompt(
+    "How many characters would you like your password to contain?"
+  );
 
-// Write an array string  
-let characterOptions = []
+  // alert message if the length characters is either true, greater than or less than
+  if (isNaN(passwordLength) === true) {
+    alert("Please use numbers for characters length");
+    return;
+  }
 
-let numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-let special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ":", ";", "'", "", "<", ",", ">", ".", "?", "/", "|", "{", "}", "[",
-"]", "<", ">",];
-let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  if (passwordLength < 8) {
+    alert("Password length must be at least 8 characters ");
+    return;
+  }
 
-// Evaluate function
-if (includeSpecial) {
-  characterOptions = characterOptions.concat(special);
-}
+  if (passwordLength > 128) {
+    alert("Password length must be at least 128 characters ");
+    return;
+  }
 
-if (includeUppercase) {
-  characterOptions = characterOptions.concat(uppercase);
-}
+  // window message
+  numericCharacters = window.confirm(
+    "Click OK to confirm including numeric characters."
+  );
 
-if (includeLowercase) {
-  characterOptions = characterOptions.concat(lowercase);
-}
+  specialCharacters = window.confirm(
+    "Click OK to confirm including special characters."
+  );
 
-// No variable added
-let res = "";
+  lowerCaseCharacters = window.confirm(
+    "Click OK to confirm including lowercase characters."
+  );
+
+  upperCaseCharacters = window.confirm(
+    "Click OK to confirm including uppercase characters."
+  );
+
+  // If user clicks cancel
+  if (
+    numericCharacters === false &&
+    specialCharacters === false &&
+    lowerCaseCharacters === false &&
+    upperCaseCharacters === false
+  ) {
+    alert("Choose a character");
+    return;
+  }
+
+  // concating to merge array
+  if (numericCharacters) {
+    characterOptions = characterOptions.concat(numeric);
+  }
+
+  if (specialCharacters) {
+    characterOptions = characterOptions.concat(special);
+  }
+
+  if (lowerCaseCharacters) {
+    characterOptions = characterOptions.concat(lowercase);
+  }
+
+  if (upperCaseCharacters) {
+    characterOptions = characterOptions.concat(uppercase);
+  }
+
+  // No variable added
+  var results = "";
 
   console.log(passwordLength);
 
-// Loop the random password
-  for (i=0; i < passwordLength; i++) {
-      let randomIndex = Math.floor(Math.random() * characterOptions.length)  
-  res += characterOptions[randomIndex]
+  // Loop the random password
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characterOptions.length);
+    results += characterOptions[randomIndex];
   }
-  return res
+  return results;
 }
 
-// Write password to the #password input
+// window prompt password to the #password input
 function writePassword() {
-    let passwordLength = window.prompt("How many characters would you like your password to contain?");
-    let includeNumeric = window.confirm("Click OK to confirm including numeric characters.");
-    let includeSpecial = window.confirm("Click OK to confirm including special characters.");
-    let includeLowercase = window.confirm("Click OK to confirm including lowercase characters.");
-    let includeUppercase = window.confirm("Click OK to confirm including uppercase characters.");
+  var password = generatePassword();
 
-// Generate password index
-  var password = generatePassword(
-    passwordLength,
-    includeNumeric,
-    includeSpecial,
-    includeLowercase,
-    includeUppercase
-    );
-  
   // Password text variable
-    var passwordText = document.querySelector("#password");
-  
+  var passwordText = document.querySelector("#password");
+
   // Add password text to generate password
-    passwordText.value = password;
-  
-  }
+  passwordText.value = password;
+}
 
-
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-
-  
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
